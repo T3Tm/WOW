181c430cd7e52a30b505213972ea0363acd70f00
Striver <chl020729@gmail.com>
2024-06-01 17:34:56 +0900

    init Commit

---
+++ WOW/WOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WOWApp.swift
+//  WOW
+//
+//  Created by choejihun on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
